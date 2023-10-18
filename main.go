5f0da032876be3fc44906608e3e0f37cea8c90ca
Jochem <33877556+Jochem323@users.noreply.github.com>
2023-10-19 00:54:55 +0200

    ping pong!

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + "MTE2NDMyMTQ0MTQzMzUzODY5MA.G4zpAX.9w1SuTsQieI1vjO6tF2l2-JceVaJrqf37P_GQY")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "pong")
+		}
+	})
+
+	discord.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Bot is running. Press CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	discord.Close()
+}
