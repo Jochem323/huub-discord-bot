94f641a8bbaedcfb4e8c6413d0489fc69b3199bb
Jochem <33877556+Jochem323@users.noreply.github.com>
2023-10-19 21:38:13 +0200

    add monke gif command

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
+
+type GifResponse struct {
+	Results []struct {
+		ID    string `json:"id"`
+		Title string `json:"title"`
+		Media struct {
+			Gif struct {
+				URL string `json:"url"`
+			} `json:"gif"`
+		} `json:"media_formats"`
+	} `json:"results"`
+}
 
 func main() {
 	discord, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
@@ -26,6 +41,27 @@ func main() {
 
 		if m.Content == "PING" {
 			s.ChannelMessageSend(m.ChannelID, "PONG")
+		}
+
+		if m.Content == "monke" {
+			url := "https://tenor.googleapis.com/v2/search?q=monke&key=" + os.Getenv("TENOR_KEY") + "&client_key=huub-discord-bot&country=NL&random=true&limit=50&media_filter=gif"
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Println(err)
+			}
+
+			respBody, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Printf("client: could not read response body: %s\n", err)
+			}
+
+			var gifResponse GifResponse
+			err = json.Unmarshal(respBody, &gifResponse)
+			if err != nil {
+				log.Println(err)
+			}
+
+			s.ChannelMessageSend(m.ChannelID, gifResponse.Results[0].Media.Gif.URL)
 		}
 	})
 
