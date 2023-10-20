3cba81947422bde7fab65cbbccbd3c450ba10e54
Jochem <33877556+Jochem323@users.noreply.github.com>
2023-10-20 16:47:30 +0200

    Add prefix command

---
+++ commands/prefixcommand.go
@@ -0,0 +1,30 @@
+package commands
+
+import (
+	"fmt"
+	"huub-discord-bot/storage"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func PrefixCommand(d *discordgo.Session, m *discordgo.MessageCreate, store storage.GuildStore) {
+	// Retrieve the guild from the database
+	guild, err := store.GetGuild(m.GuildID)
+	if err != nil {
+		return
+	}
+
+	// Retrieve the prefix from the message
+	prefix := strings.TrimSpace(strings.Split(m.Content, " ")[1])
+
+	// Update the prefix in the database
+	guild.Prefix = prefix
+	err = store.UpdateGuild(*guild)
+	if err != nil {
+		return
+	}
+
+	// Send a message to the channel
+	d.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Prefix updated to %s", prefix))
+}
